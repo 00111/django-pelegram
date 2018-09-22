93fa25c674dc866bc56dfdbde0e6765c37b91c8c
Ivan Kuznetsov <719197@gmail.com>
2018-09-22 19:47:48 +0000

    refactoring

--- bot_handler.py
+++ bot_handler.py
@@ -2,38 +2,94 @@ from pelegram.telegram import TelegramApi
 import re
 from django.http import JsonResponse
 
+
+class Request(object):
+
+    def __init__(self, payload):
+        self.payload = payload
+        if 'message' in self.payload.keys():
+            self.data = self.message_request()
+        elif 'edited_message' in self.payload.keys():
+            self.data = self.edited_message_request()
+        elif 'callback_query' in self.payload.keys():
+            self.data = self.callback_query_request()
+
+    def callback_query_request(self):
+        data = {
+            'text': self.payload['callback_query']['data'],
+            'chat_id': self.payload['callback_query']['message']['chat']['id'],
+            'callback_query_id': self.payload['callback_query']['id'],
+            'user': self.payload['callback_query']['from']['id'],
+            'type': 'callback_query'
+        }
+        return data
+
+    def message_request(self):
+        data = {
+            'text': self.payload['message']['text'],
+            'chat_id': self.payload['message']['chat']['id'],
+            'user': self.payload['message']['from']['id'],
+            'type': 'message'
+        }
+        return data
+
+    def edited_message_request(self):
+        data = {
+            'text': self.payload['edited_message']['text'],
+            'chat_id': self.payload['edited_message']['chat']['id'],
+            'user': self.payload['edited_message']['from']['id'],
+            'type': 'edited_message'
+        }
+        return data
+
+
 class BotBasic(object):
 
     def __init__(self, payload, bot_token):
         self.telegram_api = TelegramApi(bot_token)
-        self.payload = payload
-        try:
-            self.message = payload['message']
-        except KeyError:
-            self.message = payload['edited_message']
+        self.request = Request(payload)
         self.check_command()
-        #self.register_user()
+        self.allow_access = self.check_access(self.request.data['user'])
 
     def check_command(self):
-        re_command = re.match(r'^/\w+', self.message['text'])
+        re_command = re.match(r'^/\w+', self.request.data['text'])
         self.command = ""
         if re_command:
-            self.command = re_command.group().replace('/', '')
+            self.command = "bot_" + re_command.group().replace('/', '')
         self.has_requested_command = getattr(self, self.command, None)
 
     def send_message(self, message):
-        self.telegram_api.send_message(self.message['chat']['id'], message)
+        self.telegram_api.send_message(self.request.data['chat_id'], message)
+
+    def send_message_inline(self, message):
+        if self.request.data['type'] == 'callback_query':
+            self.telegram_api.send_message_inline(self.request.data['chat_id'], message,
+                                                  self.request.data['callback_query_id'])
+        else:
+            self.telegram_api.send_message_inline(self.request.data['chat_id'], message)
+
+    def send_photo(self, file_path):
+        self.telegram_api.send_photo(self.request.data['chat_id'], file_path)
 
     def dont_understand(self):
-        self.send_message("Bot don't undestand your command ¯\_(ツ)_/¯")
+        self.send_message("Bot don't understand your command ¯\_(ツ)_/¯")
 
     def telegram_response(self):
         return JsonResponse({}, status=200)
 
     def exception_occurred(self, err):
         self.send_message("Run-time error:{0}".format(err))
-    """
-    def register_user(self):
-        user = Users.objects.get_or_create(identif=self.message['chat']['id'], password='123',)
-        self.user = user
-    """
+
+    def methods_bot(self):
+        methods = []
+        for attr in dir(self):
+            if 'bot_' in attr:
+                method = getattr(self, attr)
+                methods.append("/{0} - {1}".format(attr.replace('bot_', ''), method.__doc__.strip(' \t\n\r')))
+        return methods
+
+    def check_access(self, from_id):
+        return False if from_id != 199965779 else True
+
+    def access_denied(self):
+        self.send_message("Access denied")
--- telegram.py
+++ telegram.py
@@ -1,6 +1,8 @@
 import logging
 from django.conf import settings
 import requests
+import json
+
 
 class TelegramApi(object):
 
@@ -10,7 +12,26 @@ class TelegramApi(object):
         if self.use_logging:
             self.logger = logging.getLogger('telegram_api_logs')
 
-    def send_message(self, chat_id, message):
-        telegram_request = requests.post(self.url + "sendMessage", data={"chat_id": chat_id, "text": message})
+    def request(self, method, **kwargs):
+        telegram_request = requests.post(self.url + method, **kwargs)
+        self.log(telegram_request.text)
+
+    def log(self, data):
         if self.use_logging:
-            self.logger.info(telegram_request.text)
+            self.logger.info(data)
+
+    def send_message(self, chat_id, message):
+        self.request("sendMessage", data={"chat_id": chat_id, "text": message})
+
+    def send_photo(self, chat_id, file_path):
+        files = {'photo': open(file_path, 'rb')}
+        self.request("sendPhoto", data={"chat_id": chat_id}, files=files)
+
+    def send_message_inline(self, chat_id, message, callback_query_id=False):
+        keyboard = {"inline_keyboard": message['keyboard']}
+        self.request("sendMessage",
+                     data={"chat_id": chat_id, "text": message['text'], "reply_markup": json.dumps(keyboard)})
+        if callback_query_id:
+            self.request("answerCallbackQuery",
+                         data={"callback_query_id": callback_query_id,
+                               "text": "{0} processed".format(message['called_name'])})
