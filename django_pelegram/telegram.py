fc09bdf22e85a97017f50ad08c6d8124f32442c1
Ivan Kuznetsov <719197@gmail.com>
2018-12-10 21:35:18 +0000

    rename API_URL to TELEGRAM_API_URL

--- django_pelegram/telegram.py
+++ django_pelegram/telegram.py
@@ -7,7 +7,7 @@ import json
 class TelegramApi(object):
 
     def __init__(self, api_token, use_logging=True):
-        self.url = "{0}/bot{1}/".format(settings.API_URL, api_token)
+        self.url = "{0}/bot{1}/".format(settings.TELEGRAM_API_URL, api_token)
         self.use_logging = use_logging
         if self.use_logging:
             self.logger = logging.getLogger('telegram_api_logs')
