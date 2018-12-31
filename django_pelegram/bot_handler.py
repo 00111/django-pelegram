436120cb2f44c7266d71f3994985bb42dcadab41
Ivan Kuznetsov <719197@gmail.com>
2018-12-31 09:49:40 +0000

    add Response class

--- django_pelegram/bot_handler.py
+++ django_pelegram/bot_handler.py
@@ -20,7 +20,8 @@ class Request(object):
             'chat_id': self.payload['callback_query']['message']['chat']['id'],
             'callback_query_id': self.payload['callback_query']['id'],
             'user': self.payload['callback_query']['from']['id'],
-            'type': 'callback_query'
+            'type': 'callback_query',
+            'testing_request': True if 'testing_request' in self.payload.keys() else False
         }
         return data
 
@@ -29,7 +30,8 @@ class Request(object):
             'text': self.payload['message']['text'],
             'chat_id': self.payload['message']['chat']['id'],
             'user': self.payload['message']['from']['id'],
-            'type': 'message'
+            'type': 'message',
+            'testing_request': True if 'testing_request' in self.payload.keys() else False
         }
         return data
 
@@ -38,9 +40,25 @@ class Request(object):
             'text': self.payload['edited_message']['text'],
             'chat_id': self.payload['edited_message']['chat']['id'],
             'user': self.payload['edited_message']['from']['id'],
-            'type': 'edited_message'
+            'type': 'edited_message',
+            'testing_request': True if 'testing_request' in self.payload.keys() else False
         }
         return data
+
+
+class Response(object):
+
+    def __init__(self):
+        self._data = {"method": None, "body": None}
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, params):
+        self._data['method'] = params['method']
+        self._data['body'] = params['body']
 
 
 class BotBasic(object):
@@ -48,6 +66,7 @@ class BotBasic(object):
     def __init__(self, payload=None, bot_token=None):
         self.telegram_api = TelegramApi(bot_token)
         self.request = Request(payload)
+        self.response = Response()
 
     def get_command(self):
         re_command = re.match(r'^/\w+', self.request.data['text'])
@@ -69,20 +88,12 @@ class BotBasic(object):
     def send_photo(self, file_path):
         self.telegram_api.send_photo(self.request.data['chat_id'], file_path)
 
-    def dont_understand(self):
-        self.send_message("Bot don't understand your command ¯\_(ツ)_/¯")
+    def dont_understand_message(self):
+        return "Bot don't understand your command ¯\_(ツ)_/"
 
-    def telegram_response(self):
-        return JsonResponse({}, status=200)
+    def json_response(self, data={}, status=200):
+        return JsonResponse(data, status=status)
 
     def exception_occurred(self, err):
         self.send_message("Run-time error:{0}".format(err))
-
-    def methods_bot(self):
-        methods = []
-        for attr in dir(self):
-            if 'bot_' in attr:
-                method = getattr(self, attr)
-                methods.append("/{0} - {1}".format(attr.replace('bot_', ''), method.__doc__.strip(' \t\n\r')))
-        return methods
 
